d75ea876987a71dc59f83baba01cfe61ed26be62
standridgedevelopment <standridgedevelopment@gmail.com>
2020-10-19 15:28:44 -0400

    Database created

---
+++ SocialMedia.Data/IdentityModels.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        public DbSet<User> Users { get; set; } //
+        public DbSet<Post> Posts { get; set; } //
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
+}
---
+++ SocialMedia.Services/UserServices.cs
@@ -0,0 +1,87 @@
+﻿using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+    public class UserService
+    {
+        public class CategoryService
+        {
+            public bool CreateCategory(CategoryCreate model)
+            {
+                var emptyList = new List<string>
+            {
+                " "
+            };
+                var entity = new Category()
+                {
+                    ListOfNotes = "",
+                    CategoryID = model.CategoryID,
+                    Name = model.Name
+                };
+
+                using (var ctx = new ApplicationDbContext())
+                {
+                    ctx.Categories.Add(entity);
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+            public IEnumerable<CategoryListItem> GetCategories()
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var query = ctx.Categories.Select
+                        (e => new CategoryListItem
+                        {
+                            CategoryId = e.CategoryID,
+                            Name = e.Name
+                        }
+                        );
+                    return query.ToArray();
+                }
+            }
+            public Category GetCategoryById(int id)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity = ctx.Categories.Single(e => e.CategoryID == id);
+                    return new Category
+                    {
+                        CategoryID = entity.CategoryID,
+                        Name = entity.Name,
+                        ListOfNotes = entity.ListOfNotes,
+                        NumberOfLists = entity.NumberOfLists
+
+                    };
+                }
+            }
+            public bool UpdateCategory(Category model)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity = ctx.Categories.Single(e => e.CategoryID == model.CategoryID);
+
+                    entity.Name = model.Name;
+
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+            public bool DeleteCategory(int id)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity = ctx.Categories.Single(e => e.CategoryID == id);
+
+                    ctx.Categories.Remove(entity);
+
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+        }
+
+    }
+}
