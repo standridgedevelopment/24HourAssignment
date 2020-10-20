23a96398be75847fd13cdf84b005e891bbd73cd8
dfdevaney <dfdevaney@gmail.com>
2020-10-20 12:20:14 -0500

    More Comments Sections Updated!

--- SocialMedia.Services/CommentServices.cs
+++ SocialMedia.Services/CommentServices.cs
@@ -1,26 +1,77 @@
-﻿using SocialMedia.Data;
+﻿using Microsoft.TeamFoundation.Discussion.Client;
+using SocialMedia.Data;
 using SocialMedia.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Comment = SocialMedia.Data.Comment;
 
 namespace SocialMedia.Services
 {
-    public class CommentServices
+    public class CommentService
     {
+        private readonly int _commentID;
+        public CommentService(int commentID)
+        {
+            _commentID = commentID;
+        }
         public bool CreateComment(CommentCreate model)
         {
             var entity =
-    new Comment()
-    {
-        Text = model.Text,
-    };
+            new Comment()
+            {
+                Text = model.Text,
+            };
 
             using (var ctx = new ApplicationDbContext())
             {
                 ctx.Comments.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CommentListItem> GetComment()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Comments
+                        .Where(e => e.CommentID == _commentID)
+                        .Select(
+                            e =>
+                                new CommentListItem
+                                {
+                                    CommentID = e.CommentID,
+                                    Text = e.Text,
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public bool UpdateComment(CommentEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Comments.Single(e => e.CommentID == _commentID);
+
+                entity.Text = model.Text;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteComment(string comment)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Comments.Single(e => e.Text == comment && e.CommentID == _commentID);
+
+                ctx.Comments.Remove(entity);
+
                 return ctx.SaveChanges() == 1;
             }
         }
---
+++ SocialMedia.WebAPI/Controllers/CommentController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Data;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SocialMedia.WebAPI.Controllers
+{
+    public class CommentController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            CommentService commentService = CreateCommentService();
+            var comment = commentService.GetComment();
+            return Ok(comment);
+        }
+
+        public IHttpActionResult Post(CommentCreate comment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCommentService();
+
+            if (!service.CreateComment(comment))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(CommentEdit comment)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var service = CreateCommentService();
+
+            if (!service.UpdateComment(comment)) return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string id)
+        {
+            var service = CreateCommentService();
+
+            if (!service.DeleteComment(id)) return InternalServerError();
+
+            return Ok();
+        }
+
+        private CommentService CreateCommentService()
+        {
+            var commentID = int.Parse(User.Identity.GetUserId());
+            var commentService = new CommentService(commentID);
+            return commentService;
+        }
+
+
+    }
+}
