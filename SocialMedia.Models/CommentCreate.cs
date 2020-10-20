f403301867f1a4332e7195df8aa1a0c6af520276
dfdevaney <dfdevaney@gmail.com>
2020-10-20 10:25:40 -0500

    Partial Comment Update

--- SocialMedia.Models/CommentCreate.cs
+++ SocialMedia.Models/CommentCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace SocialMedia.Models
 {
     class CommentCreate
     {
-        // Here's a comment!
-
+        [MaxLength(10000)]
+        public string Text { get; set; }
     }
 }
