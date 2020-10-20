59144cdfbe26191f9f5e1924420fbed6334e188f
dfdevaney <dfdevaney@gmail.com>
2020-10-20 14:11:26 -0500

    Clean up some wierd additions

--- SocialMedia.Services/CommentServices.cs
+++ SocialMedia.Services/CommentServices.cs
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 ﻿using Microsoft.TeamFoundation.Discussion.Client;
 using SocialMedia.Data;
 using SocialMedia.Models;
 using System;
-=======
-﻿using System;
->>>>>>> Isaiah
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using Comment = SocialMedia.Data.Comment;
 
 namespace SocialMedia.Services
@@ -80,12 +75,6 @@ namespace SocialMedia.Services
                 return ctx.SaveChanges() == 1;
             }
         }
-=======
 
-namespace SocialMedia.Services
-{
-    public class CommentServices
-    {
->>>>>>> Isaiah
     }
 }
